b83678e7a2610b5f692f04a5715b26c72983d48e
eselse <bpytc23up@gmail.com>
2020-07-13 00:56:19 +0400

    change url

--- js/app.js
+++ js/app.js
@@ -1072,7 +1072,8 @@ for (let i = 0; i < linkList.length; i++) {
   linkList[i].addEventListener("click", function () {
     let title = linkList[i].textContent;
     let id = orderedMovieList[title];
-    document.URL = `https://vid${Date.now()}vb17110frankfoley.pw/api/idkp?kp_id=${id}`;
+    let url = `https://vid${Date.now()}.vb17110frankfoley.pw/api/idkp?kp_id=${id}`;
+    window.location.replace(url);
     // let iframe = `<iframe width="700" height="500" src="${url}" frameborder="0" allowfullscreen=""></iframe>`;
     // document.querySelector(".movieContainer").innerHTML = iframe;
     let header = document.createElement("h1");
